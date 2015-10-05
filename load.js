08d2e2e888d438cc420cd2687ea2087e813a1a76
hudasanca <hudasanca@gmail.com>
2015-10-05 17:18:02 +0700

    Im deleting all of these files

--- load.js
+++
@@ -1,8 +0,0 @@
-var monthly = null;
-
-$(document).ready(function(){
-  chrome.storage.local.get('monthly', function(result){
-    monthly = result.monthly;
-    $("#title").html(monthly.title);
-  });
-});
